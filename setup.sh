16ea9c13ffd14bf5b7bac2d7d1b38674fb892e5c
keelbeelveel <k.belveal17@gmail.com>
2022-11-28 16:40:03 -0500

    added valgrind, stdman

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt update
 sudo apt upgrade -y
-sudo apt install -y apt-transport-https bitwise curl dotnet6 gcc gdb g++ git gpg lf neovim nodejs openssl python3 ssh tar tmux wget zsh
+sudo apt install -y apt-transport-https bitwise curl dotnet6 gcc gdb g++ git gpg lf neovim nodejs openssl python3 ssh tar tmux valgrind wget zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting 
 cp raw.zshrc ~/.zshrc
@@ -9,14 +9,14 @@ cp raw.zshrc ~/.zshrc
 chsh /bin/zsh
 
 # Setup VSCode Keyring
-wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
-sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
-rm -f packages.microsoft.gpg
+#wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+#sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+#sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+#rm -f packages.microsoft.gpg
 
 # Install VSCode
-sudo apt update
-sudo apt install -y code
+#sudo apt update
+#sudo apt install -y code
 
 # Setup Vim Plugged
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
@@ -39,3 +39,11 @@ wget https://raw.githubusercontent.com/reelbeelveel/miamineon/master/miamineon-a
 mkdir -p ~/.vim/colors && mv miamineon.vim ~/.vim/colors/miamineon.vim
 mkdir -p ~/.vim/plugged/vim-airline-themes/autoload/airline/themes/ && cp miamineon-airline.vim ~/.vim/plugged/vim-airline-themes/autoload/airline/themes/miamineon.vim
 mv miamineon-airline.vim ~/.config/nvim/plugged/vim-airline-themes/autoload/airline/themes/miamineon.vim
+
+# stdman setup
+git clone https://github.com/jeaye/stdman.git
+cd stdman
+./configure
+sudo make install
+cd ..
+rm -rvf stdman
